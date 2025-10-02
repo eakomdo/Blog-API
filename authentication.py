c7f8bb8829d97b74c0e641f14d14884b3a4d981f
Emmanuel <akomdoemmanuel22@gmail.com>
2025-10-02 01:32:01 +0000

    creating a blog API with python flask

---
+++ authentication.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class Registration(FlaskForm):
+    class Meta:
+        csrf = False  
+        
+    first_name = StringField(validators=[DataRequired(), Length(min=6, max=100)])
+    last_name = StringField(validators=[DataRequired(), Length(min=6, max=100)])
+    username = StringField(validators=[DataRequired(), Length(min=6, max=100)])
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired()])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password')])
+    
+    
+class Login(FlaskForm):
+    class Meta:
+        csrf = False 
+        
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired()])
